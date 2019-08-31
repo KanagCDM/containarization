b80b0a1b4c7e2166b04bdc37571ecc39ef08391c
Sri Hari <ganeshkrishnacdm@outlook.com>
2019-08-31 11:18:03 +0530

    added key generation code snippets

--- service.go
+++ service.go
@@ -5,12 +5,32 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/asn1"
+	"encoding/gob"
+	"encoding/pem"	
+	"os"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	reader := rand.Reader
+	bitSize := 2048
 
+	key, err := rsa.GenerateKey(reader, bitSize)
+	checkError(err)
+
+	publicKey := key.PublicKey
+
+	saveGobKey("private.key", key)
+	savePEMKey("private.pem", key)
+
+	saveGobKey("public.key", publicKey)
+	savePublicPEMKey("public.pem", publicKey)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
@@ -20,4 +40,53 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
+}
+
+
+func saveGobKey(fileName string, key interface{}) {
+	outFile, err := os.Create(fileName)
+	checkError(err)
+	defer outFile.Close()
+
+	encoder := gob.NewEncoder(outFile)
+	err = encoder.Encode(key)
+	checkError(err)
+}
+
+func savePEMKey(fileName string, key *rsa.PrivateKey) {
+	outFile, err := os.Create(fileName)
+	checkError(err)
+	defer outFile.Close()
+
+	var privateKey = &pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+
+	err = pem.Encode(outFile, privateKey)
+	checkError(err)
+}
+
+func savePublicPEMKey(fileName string, pubkey rsa.PublicKey) {
+	asn1Bytes, err := asn1.Marshal(pubkey)
+	checkError(err)
+
+	var pemkey = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	pemfile, err := os.Create(fileName)
+	checkError(err)
+	defer pemfile.Close()
+
+	err = pem.Encode(pemfile, pemkey)
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
 }
